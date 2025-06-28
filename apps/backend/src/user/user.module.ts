a42c9d6a4e6741ef667ff98646860d710d6fc3b5
tranduc <ductran@emddi.com>
2025-06-28 21:37:06 +0700

    🔧 Fix NestJS dependency injection: Add UserVocabulary to UserModule - Fixed: Nest can't resolve dependencies of UserService - Added UserVocabulary entity to UserModule imports - UserService now can inject UserVocabularyRepository successfully - Backend will start without dependency injection errors

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from '../database/entities/user.entity';
+import { UserVocabulary } from '../database/entities/user-vocabulary.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserVocabulary])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
